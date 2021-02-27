75c75c1de93796c9cdec8535e50058593e044431
chc72331 <chc72331@creighton.edu>
2021-02-27 09:33:19 -0600

    Create ifElse.js

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,11 @@
+// Establish input some variables
+userState = prompt('What state are you in (2 capitalized letters, e.g. "NE, KS")?')
+userFirstName = prompt('What is your first name?')
+userLastName = prompt('What is your last name?')
+userTemperature = prompt('What\'s the current temperature (in Fahrenheit)?')
+
+// Establish an array that holds all the messages
+messages = ['wear a warm coat, hat, scarf, and gloves.', 'wear a warm coat but you
+  won\'t need a hat, scarf or gloves, 'wear your warmest coat, a warm hat, a scarf,
+  and warm gloves', 'wear a warm coat, hat and gloves.  Maybe a scarf too.']
+  
