ea39372f8665fea277eeb1a7de3287106cab8c08
chc72331 <chc72331@creighton.edu>
2021-02-27 14:42:45 -0600

    Update ifElse.js

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,11 +1,15 @@
 // Establish input some variables
-userState = prompt('What state are you in (2 capitalized letters, e.g. "NE, KS")?')
-userFirstName = prompt('What is your first name?')
-userLastName = prompt('What is your last name?')
-userTemperature = prompt('What\'s the current temperature (in Fahrenheit)?')
+let userState = prompt('What state are you in (2 capitalized letters, e.g. "NE, KS")?')
+let userFirstName = prompt('What is your first name?')
+let userLastName = prompt('What is your last name?')
+let userTemperature = prompt('What\'s the current temperature (in Fahrenheit)?')
 
 // Establish an array that holds all the messages
-messages = ['wear a warm coat, hat, scarf, and gloves.', 'wear a warm coat but you
-  won\'t need a hat, scarf or gloves, 'wear your warmest coat, a warm hat, a scarf,
+let messages = ['wear a warm coat, hat, scarf, and gloves.', "wear a warm coat but you
+  won't need a hat, scarf or gloves.", 'wear your warmest coat, a warm hat, a scarf,
   and warm gloves', 'wear a warm coat, hat and gloves.  Maybe a scarf too.']
+console.log(messages)
+
+var loo = 'won\'t'
+console.log(string)
   
