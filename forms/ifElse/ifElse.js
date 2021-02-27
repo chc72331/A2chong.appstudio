4c5770c6d6be408f5e2be3f79a4611765423cdeb
chc72331 <chc72331@creighton.edu>
2021-02-27 15:10:22 -0600

    Update ifElse.js

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,15 +1,25 @@
 // Establish input some variables
-let userState = prompt('What state are you in (2 capitalized letters, e.g. "NE, KS")?')
+let userState = prompt('What state are you in (2 capitalized letters, e.g. "NE" or "FL")?')
 let userFirstName = prompt('What is your first name?')
 let userLastName = prompt('What is your last name?')
 let userTemperature = prompt('What\'s the current temperature (in Fahrenheit)?')
 
 // Establish an array that holds all the messages
-let messages = ['wear a warm coat, hat, scarf, and gloves.', "wear a warm coat but you
-  won't need a hat, scarf or gloves.", 'wear your warmest coat, a warm hat, a scarf,
-  and warm gloves', 'wear a warm coat, hat and gloves.  Maybe a scarf too.']
-console.log(messages)
+let messages = ['wear a warm coat, hat, scarf, and gloves.', 
+  'wear a warm coat but you won\'t need a hat, scarf or gloves.', 
+  'wear your warmest coat, a warm hat, a scarf, and warm gloves', 
+  'wear a warm coat, hat and gloves.  Maybe a scarf too.']
 
-var loo = 'won\'t'
-console.log(string)
-  
+var messageOut
+if (userTemperature < 32 && userState == 'NE')
+  messageOut = messages[0]
+else if (userTemperature < 50 && userTemperature >= 32 && userState == 'NE')
+  messageOut = messages[1]
+else if (userTemperature < 50 && userTemperature >= 32 && userState == 'FL')
+  messageOut = messages[2]
+else if (userTemperature < 70 && userTemperature >= 50 && userState == 'FL')
+  messageOut = messages[3]
+else
+  console.log('Go check your local weather station.')
+
+console.log(messageOut)
